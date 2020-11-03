7827c8b86f70c159f22218403e09661acbfc7b61
Kevin Gessner <kgessner@etsy.com>
2020-11-03 14:22:54 +0000

    drop support for python2 and upgrade rules_python/rules_docker

--- test/dash.py
+++ test/dash.py
@@ -2,12 +2,9 @@ from grafanalib.core import *
 from grafanalib._gen import print_dashboard
 import sys
 
-# this should fail if loaded in python3
-if b'test' != 'test':
+# this should fail if loaded in python2
+if b'test' == 'test':
     sys.exit(1)
-d = {'a': 1, 'b': 2}
-for key, value in d.iteritems():
-    continue
 
 dashboard = Dashboard(
     title="Python sample",
